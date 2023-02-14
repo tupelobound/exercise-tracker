e59d316297d578183e725a78bba9e7214d86db2b
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-14 18:45:55 +0000

    Added HTML and favicon

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+// set up
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// serve static assets from the 'public' directory
+app.use(express.static('public'))
 
+// listen for requests
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
