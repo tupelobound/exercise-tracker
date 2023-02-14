c43a5aab418f88c09f43c05967207aea2435c1e5
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-14 19:32:41 +0000

    Create schemas and models

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const shortid = require('shortid')
 const port = 3000
 
 // connect to mongodb database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Create mongoose schema for user
+const Schema = mongoose.Schema;
+var userSchema = new Schema({
+  _id: { 'type': String,'default': shortid.generate },
+  username: String
+});
+
+// Create user model
+var User = mongoose.model('User', userSchema);
+
+// Create mongoose schema for workout
+var workoutSchema = new Schema({
+  userID: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+// Create workout model
+var Workout = mongoose.model('Workout', workoutSchema);
 
 // serve static assets from the 'public' directory
 app.use(express.static('public'))
