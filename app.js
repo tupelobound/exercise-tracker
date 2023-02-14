1837a9ac83e329e989d04aa5d50363ab1ef1e2ed
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-14 19:04:35 +0000

    Add database connection

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // set up
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
+
+// connect to mongodb database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // serve static assets from the 'public' directory
 app.use(express.static('public'))
