6dd0deeebd60ce2f399429e7652a2c3d942d8f8e
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-14 19:52:08 +0000

    Added new user functionality

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const shortid = require('shortid')
+const bodyParser = require('body-parser')
 const port = 3000
+
+// Mount the body parser
+app.use(bodyParser.urlencoded({extended: false}))
 
 // connect to mongodb database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -12,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // Create mongoose schema for user
 const Schema = mongoose.Schema;
 var userSchema = new Schema({
-  _id: { 'type': String,'default': shortid.generate },
+  _id: { 'type': String, 'default': shortid.generate },
   username: String
 });
 
@@ -32,6 +36,24 @@ var Workout = mongoose.model('Workout', workoutSchema);
 
 // serve static assets from the 'public' directory
 app.use(express.static('public'))
+
+// Add new user from POST
+app.post('/api/exercise/new-user', (req, res) => {
+
+  // check if new user exists in the database
+  User.find({ username: req.body.username }, (err, result) => {
+    // if not, create and save a new user
+    if (result.length === 0) {
+      var newUser = new User({ username: req.body.username });
+      newUser.save((err, result) => {
+        res.json({ username: result.username, _id: result._id })
+      });
+      // if so, send the user a message  
+    } else {
+      res.send('Sorry! Username already taken');
+    }
+  });
+});
 
 // listen for requests
 app.listen(port, () => {
